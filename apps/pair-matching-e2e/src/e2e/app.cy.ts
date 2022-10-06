f88dcc64ff11cc45e033998fa9b0c7e181008160
Piotr Leszczyński <piotr@leszczynski.it>
2022-10-06 12:19:03 +0200

    Initial commit

---
+++ apps/pair-matching-e2e/src/e2e/app.cy.ts
@@ -0,0 +1,13 @@
+import { getGreeting } from '../support/app.po';
+
+describe('pair-matching', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should display welcome message', () => {
+    // Custom command example, see `../support/commands.ts` file
+    cy.login('my-email@something.com', 'myPassword');
+
+    // Function helper example, see `../support/app.po.ts` file
+    getGreeting().contains('Welcome pair-matching');
+  });
+});
